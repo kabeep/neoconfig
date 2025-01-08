7a987aeafad62f2fa63baf0396209e17c37a3269
kabeep <zzx2067@outlook.com>
2025-01-09 07:09:17 +0800

    perf: Embed brand ASCII art into the prompt component

--- src/components/prompt.ts
+++ src/components/prompt.ts
@@ -14,7 +14,7 @@ import {
 } from '@inquirer/core';
 import type { PartialDeep } from '@inquirer/type';
 import { FIGURES } from '../constants';
-import { palette } from '../utils';
+import { i18n, palette } from '../utils';
 
 type SearchTheme = {
     icon: { cursor: string };
@@ -113,6 +113,25 @@ function normalizeChoices<Value>(
             disabled: choice.disabled ?? false,
         };
     });
+}
+
+function brandAsciiArt() {
+    return `
+     ⠐⢷⣦⣄⣀⢀⣀⣀⣤⣤⣤⣀⣀⠀⣀⣤⣶⡶
+      ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏          ${palette.cyan('_   _             ____             __ _')}
+       ⢰⣿⣿⣿⣿⣿⣿⡟⠉⣉⡺⣿⣿⣧⡀         ${palette.cyan('| \\ | | ___  ___  / ___|___  _ __  / _(_) __ _')}
+       ⢸⣿⣿⣿⣿⣿⡿${palette.yellow('⢀⡾⣯⣷⣹')}⣿⣿⣿⣶⣦⡀      ${palette.cyan("|  \\| |/ _ \\/ _ \\| |   / _ \\| '_ \\| |_| |/ _` |")}
+      ⣠⣿⣿⣿⣿⣿⣿⣇${palette.yellow('⠈⠂⢹⣿⢧')}⣿⣿⣿⡿⠋       ${palette.cyan('| |\\  |  __/ (_) | |__| (_) | | | |  _| | (_| |')}
+    ⠠⣾⣿⣿⣿⣿⠿⣿⣿⣿⣷⣮⣭⣭⣽⣿⣿⡿⠁        ${palette.cyan('|_| \\_|\\___|\\___/ \\____\\___/|_| |_|_| |_|\\__, |')}
+     ⣿⣿⣿⣿⠃ ⡻⣿⣿⣿⡟${palette.yellow('⣛⣛⣋')}             ${palette.magenta('A NeoFetch configuration file manager.')}  ${palette.cyan('|___/')}
+     ⠙⠻⢿⣿⣶⣞⣛⣭⣙⣟${palette.yellow('⣼⣿⣿⣍⡳⣦⣀')}
+      ${palette.yellow('⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣿⣷⣦⣀')}       ${i18n.t('CMD_TIP.MOVE_CURSOR', { key: `${palette.yellow(FIGURES.ARROW_TOP)}  / ${palette.yellow(FIGURES.ARROW_BOTTOM)} ` })}
+    ${palette.yellow('⣠⣾⣿⣿⣟⣽⣿⣿')}⣿⣿⣿⣿⣿⣿⣿⣿⣿⡌${palette.yellow('⠛⢿⣿⡷')}      ${i18n.t('CMD_TIP.ENTER_DIRECTORY', { key: `${palette.yellow(FIGURES.ARROW_RIGHT)}  / ${palette.yellow('ENTER')}` })}
+  ${palette.yellow('⠐⡦')}⣨⣾⢚${palette.yellow('⣷⣿⣿⣿')}⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿${palette.yellow('⣳⣷⡀')}        ${i18n.t('CMD_TIP.RETURN_PARENT', { key: `${palette.yellow(FIGURES.ARROW_LEFT)} ` })}
+   ⣱⣿⡃${palette.yellow('⢸⣿⣿⣿⣿')}⣿⣿⣿⣿⣿⣿⣿⣿⠟${palette.yellow('⣱⣿⣿⠃')}        ${i18n.t('CMD_TIP.SWITCH_THEME', { key: palette.yellow('ENTER') })}
+⣀⣤⣾⣿⡇ ${palette.yellow('⢸⣿⣿⣿⣿⠟')}⠵⠿⠿⠟⠛⠋${palette.yellow('⠵⠿')}⢿⡿⣃         ${i18n.t('CMD_TIP.EXIT', { key: palette.yellow('CTRL - C') })}
+⠻⠛⠛⠿⠇ ⠈⠻⡿⠉
+`;
 }
 
 export default createPrompt(
@@ -303,7 +322,9 @@ export default createPrompt(
             : '';
 
         return [
-            [prefix, message, searchStr].filter(Boolean).join(' '),
+            [brandAsciiArt(), '\n', prefix, message, searchStr]
+                .filter(Boolean)
+                .join(' '),
             `${error ?? page}${helpTip}${choiceDescription}`,
         ];
     },
